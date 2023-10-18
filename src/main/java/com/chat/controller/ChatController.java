b84b7b759a0cdbb7c2e15c8d528c65759cdd7c06
성채은 <schn1115@kakao.com>
2023-10-18 15:52:55 +0900

    chat commit

---
+++ src/main/java/com/chat/controller/ChatController.java
@@ -0,0 +1,45 @@
+package com.chat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatController {
+    private static final List<Session> session = new ArrayList<Session>();
+
+    @GetMapping("/chat")
+    public String main() {
+        return "chat/chat";
+    }
+
+    @OnOpen
+    public void open(Session newUser) {
+        System.out.println("connected");
+        session.add(newUser);
+        System.out.println(newUser.getId());
+    }
+
+    @OnMessage
+    public void getMsg(Session receiveSession, String msg) {
+        for (int i = 0; i < session.size(); i++) {
+            if (!receiveSession.getId().equals(session.get(i).getId())) {
+                try {
+                    session.get(i).getBasicRemote().sendText("상대: " + msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    session.get(i).getBasicRemote().sendText("나: " + msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

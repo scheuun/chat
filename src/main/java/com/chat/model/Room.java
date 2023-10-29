66ca7637476679f0e7f09346bf22993766ff5758
성채은 <schn1115@kakao.com>
2023-10-29 21:29:13 +0900

    mapper 작성

--- src/main/java/com/chat/model/Room.java
+++ src/main/java/com/chat/model/Room.java
@@ -3,6 +3,7 @@ package com.chat.model;
 public class Room {
     private int room_id;
     private String room_name;
+    private String creator_id ;
 
     public int getRoom_id() {
         return room_id;
@@ -18,5 +19,13 @@ public class Room {
 
     public void setRoom_name(String room_name) {
         this.room_name = room_name;
+    }
+
+    public String getCreator_id() {
+        return creator_id;
+    }
+
+    public void setCreator_id(String creator_id) {
+        this.creator_id = creator_id;
     }
 }

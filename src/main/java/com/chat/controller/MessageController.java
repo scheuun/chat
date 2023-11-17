f734c77d9330e46a952cf11e64a56609ce40e9d6
성채은 <schn1115@kakao.com>
2023-11-17 16:29:46 +0900

    채팅 기능 중

--- src/main/java/com/chat/controller/MessageController.java
+++ src/main/java/com/chat/controller/MessageController.java
@@ -2,7 +2,6 @@ package com.chat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -10,11 +9,14 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 @ServerEndpoint("/chat")
 public class MessageController {
     private static final List<Session> session = new ArrayList<Session>();
+    private static final Map<String, List<Session>> roomSessions = new ConcurrentHashMap<>();
 
     @GetMapping("/chat/chat")
     public String chat() {
@@ -28,22 +30,52 @@ public class MessageController {
         System.out.println(newUser.getId());
     }
 
+//    @OnMessage
+//    public void getMsg(Session receiveSession, String msg) {
+//        for (int i = 0; i < session.size(); i++) {
+//            if (!receiveSession.getId().equals(session.get(i).getId())) {
+//                try {
+//                    session.get(i).getBasicRemote().sendText("상대: " + msg);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } else {
+//                try {
+//                    session.get(i).getBasicRemote().sendText("나: " + msg);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
+
+
     @OnMessage
-    public void getMsg(Session receiveSession, String msg) {
-        for (int i = 0; i < session.size(); i++) {
-            if (!receiveSession.getId().equals(session.get(i).getId())) {
+    public void getMsg(Session receiveSession, String message) {
+        String roomNum = extractRoomNum(receiveSession.getRequestURI().getQuery());
+        if (roomNum != null) {
+            List<Session> sessionsInRoom = roomSessions.computeIfAbsent(roomNum, k -> new ArrayList<>());
+
+            for (Session session : sessionsInRoom) {
                 try {
-                    session.get(i).getBasicRemote().sendText("상대: " + msg);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                try {
-                    session.get(i).getBasicRemote().sendText("나: " + msg);
+                    String sender = (session.getId().equals(receiveSession.getId())) ? "나" : "상대";
+                    session.getBasicRemote().sendText(sender + ": " + message);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    private String extractRoomNum(String query) {
+        String[] params = query.split("&");
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            if (keyValue.length == 2 && "room_num".equals(keyValue[0])) {
+                return keyValue[1];
+            }
+        }
+        return null;
     }
 }

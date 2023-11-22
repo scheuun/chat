6bd333430eb9cc7bc8a04dd12950d10ee13b8897
성채은 <schn1115@kakao.com>
2023-11-22 16:16:07 +0900

    아이디 확인 로직 추가 중

--- src/main/java/com/chat/controller/MessageController.java
+++ src/main/java/com/chat/controller/MessageController.java
@@ -1,15 +1,19 @@
 package com.chat.controller;
 
+import com.chat.model.Room;
+import com.chat.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,6 +21,9 @@ import java.util.concurrent.ConcurrentHashMap;
 @Controller
 @ServerEndpoint("/chat")
 public class MessageController {
+    @Autowired
+    RoomService roomService;
+
     private static final Map<String, Session> activeSessions = new ConcurrentHashMap<>();
 
     @GetMapping("/chat/chat")
@@ -24,13 +31,24 @@ public class MessageController {
         return "chat/chat";
     }
 
-
-
     @OnOpen
-    public void open(Session session) {
-        System.out.println("connected");
-        activeSessions.put(session.getId(), session);
-        System.out.println(session.getId());
+    public void open(Session session, HttpSession httpSession, HttpServletRequest request) {
+//        System.out.println("connected");
+//        activeSessions.put(session.getId(), session);
+//        System.out.println(session.getId());
+
+        if (isValid(httpSession, request)) {
+            System.out.println("connected");
+            activeSessions.put(session.getId(), session);
+            System.out.println(session.getId());
+        } else {
+            System.out.println("unconnected");
+            try {
+                session.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @OnClose
@@ -55,5 +73,18 @@ public class MessageController {
                 }
             }
         }
+    }
+
+    private boolean isValid (HttpSession httpSession, HttpServletRequest request) {
+        List<Room> roomList = roomService.selectRoom(request.getParameter("room_num"));
+        boolean result = false;
+
+        for (int i = 0; i < roomList.size(); i++) {
+            String creator_id = roomList.get(i).getCreator_id();
+            String invitee_id = roomList.get(i).getInvitee_id();
+            if (httpSession.getAttribute("id").equals(creator_id)
+                    || httpSession.getAttribute("id").equals(invitee_id)) result = true;
+        }
+        return result;
     }
 }

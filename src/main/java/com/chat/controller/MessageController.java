0680de59a8868a691ed5378497c32734260b54e9
성채은 <schn1115@kakao.com>
2023-12-15 15:09:49 +0900

    채팅 사용자 구분 개발 중

--- src/main/java/com/chat/controller/MessageController.java
+++ src/main/java/com/chat/controller/MessageController.java
@@ -4,12 +4,16 @@ import com.chat.model.Room;
 import com.chat.service.MemberService;
 import com.chat.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
@@ -17,6 +21,7 @@ import javax.websocket.server.ServerEndpointConfig;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -34,8 +39,9 @@ public class MessageController extends Socket {
     private static final Map<String, Session> activeSessions = new ConcurrentHashMap<>();
 
     @GetMapping("/chat/chat")
-    public String chat() {
-        return "chat/chat";
+    public String chat(HttpSession session, @RequestParam("room_num") int room_num) {
+        String id = (String) session.getAttribute("id");
+        return  roomService.isMemRoom(room_num, id, id) > 0 ? "chat/chat" : "";
     }
 
     @OnClose
@@ -54,15 +60,14 @@ public class MessageController extends Socket {
     }
 
     @OnMessage
-    public void getMsg(Session recieveSession, String msg, @PathParam("room_num") int room_num) {
+    public void getMsg(Session recieveSession, String msg) {
         String senderId = recieveSession.getId();
-        System.out.println(room_num);
 
         for (Session session : activeSessions.values()) {
-            String targetUserId = (String) session.getUserProperties().get("id");
+//            String targetUserId = (String) session.getUserProperties().get("id");
 
             // 방에 속한 사용자들 간에만 메시지 전송
-            if (targetUserId != null && roomService.isMemRoom(room_num, targetUserId, targetUserId) > 0) {
+//            if (targetUserId != null && roomService.isMemRoom(room_num, targetUserId, targetUserId) > 0) {
                 try {
                     if (session.getId().equals(senderId)) {
                         // 메시지를 보낸 사람에게는 '나'로 표시
@@ -74,7 +79,10 @@ public class MessageController extends Socket {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
         }
+    }
+
+    private boolean isValid(@RequestParam("room_num") int room_num, @SessionAttribute("id") String id) {
+        return roomService.isMemRoom(room_num, id, id) > 0 ? true : false;
     }
 }
